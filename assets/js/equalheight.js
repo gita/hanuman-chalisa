421ade87a9e6c17f7a56bf1293e2767622f9e3ff
Samanyou Garg <samanyugarg@gmail.com>
2017-11-12 11:22:48 +0000

    RadhaKrishna

---
+++ assets/js/equalheight.js
@@ -0,0 +1,3 @@
+var App=function(){$.fn.hasAttr=function(name){return this.attr(name)!==undefined;};function handleEqualHeightColumns(){var EqualHeightColumns=function(){$('.equal-height-row').each(function(){heights=[];$('.equal-height-column',this).each(function(){$(this).removeAttr('style');heights.push($(this).height());});$('.equal-height-column',this).height(Math.max.apply(Math,heights));});}
+EqualHeightColumns();$(window).resize(function(){EqualHeightColumns();});$(window).load(function(){EqualHeightColumns();});}
+return{init:function(){handleEqualHeightColumns();}};}();$(function(){App.init();});
